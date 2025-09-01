880491485f996c5451de136410ce331c3e60d63e
nguyentd01 <112261010+nguyentd01@users.noreply.github.com>
2025-09-02 01:16:13 +0700

    Update main.py
    Fix errors
    Add description
    Optimize speed and OCR quality

--- main.py
+++ main.py
@@ -4,59 +4,53 @@ from ebooklib import epub
 import asyncio
 from user_agent import get
 from tqdm.asyncio import tqdm
-import backoff
 from playwright.async_api import async_playwright
 import pytesseract
 from PIL import Image
+from PIL import ImageEnhance
 import io
 from appdirs import *
 import configparser
 import os.path
 import gc
-from async_lru import alru_cache
 
 
 
-# Enable garbage collection
 gc.enable()
-
 data_dir = user_config_dir(appname='metruyencv-downloader',appauthor='nguyentd010')
+print(data_dir)
 os.makedirs(data_dir, exist_ok=True)
 if not os.path.isfile(data_dir + '\config.ini'):
     config = configparser.ConfigParser()
     with open(data_dir + '\config.ini', 'w') as configfile:
         config.write(configfile)
 
-missing_chapter = []
-if os.stat(data_dir+"\config.ini").st_size == 0:
-    username = str(input('Email tài khoản metruyencv?:'))
-    password = str(input('Password?:'))
-    disk = str(input('Ổ đĩa lưu truyện(C/D):')).capitalize()
-    max_connections = int(input('''Max Connections (10 -> 1000) 
-    Note: Càng cao thì rủi ro lỗi cũng tăng, chỉ số tối ưu nhất là 50 : '''))
-    save = str(input('Lưu config?(Y/N):')).capitalize()
-
-else:
+try:
     config = configparser.ConfigParser()
     config.read(data_dir + '\config.ini')
     username = str(config.get('data', 'login'))
     password = str(config.get('data', 'password'))
     disk = str(config.get('data', 'disk'))
-    max_connections = int(config.get('data', 'max-connection'))
+    max_tabs = int(config.get('data', 'max-tabs'))
     save = None
+except:
+    username = str(input('Email tài khoản metruyencv?:'))
+    password = str(input('Password?:'))
+    disk = str(input('Ổ đĩa lưu truyện(C/D):')).capitalize()
+    max_tabs = int(input('''Number of Browser tabs:'''))
+    save = str(input('Lưu config?(Y/N):')).capitalize()
 
 
-limits = httpx.Limits(max_keepalive_connections=100, max_connections=max_connections)
+
 timeout = httpx.Timeout(None)
-client = httpx.AsyncClient(limits=limits, timeout=timeout)
+client = httpx.AsyncClient(timeout=timeout)
 
 # Base URL for the novel
-BASE_URL = 'https://metruyencv.info/truyen/'
+BASE_URL = 'https://metruyencv.com/truyen/'
 
 user_agent = get()
 
 file_location = os.getcwd()
-
 
 pytesseract.pytesseract.tesseract_cmd = fr'{file_location}\Tesseract-OCR\tesseract.exe'
 
@@ -64,16 +58,17 @@ header = {'user-agent': user_agent}
 
 if save == 'Y':
     config = configparser.ConfigParser()
-    config['data'] = {'login': username, 'password': password, 'disk' : disk, 'max-connection' : max_connections}
+    config['data'] = {'login': username, 'password': password, 'disk' : disk, 'max-tabs' : max_tabs}
 
     # Write the configuration to a file
     with open(data_dir + '\config.ini', 'w') as configfile:
         config.write(configfile)
 
+
 def ocr(image: bytes) -> str:
     image = Image.open(io.BytesIO(image))
-    image = image.convert('L')
-    text = pytesseract.image_to_string(image, lang='vie')
+    converted_img = ImageEnhance.Contrast(image.convert("L")).enhance(1.5)
+    text = pytesseract.image_to_string(converted_img, lang='vie')
     return text
 
 def delete_dupe(list):
@@ -93,54 +88,64 @@ async def handle_route(route):
   else:
     await route.continue_()
 
+async def login(context,novel_url):
+    page = await context.new_page()
+    await page.goto(novel_url,timeout=0)
+    await page.locator('xpath=/html/body/div[1]/header/div/div/div[3]/button').click()
+    await page.locator(
+        'xpath=/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div/div[1]/button').click()
+    await page.locator('xpath=/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/input').fill(username)
+    await page.locator('xpath=/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/input').fill(password)
+    await page.locator('xpath=/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[3]/div[1]/button').click()
+    await page.locator('xpath=/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[2]/button').click()
+    await page.locator('xpath=/html/body/div[1]/div[1]/div/main/div[3]/div[1]/button[1]').click()
+    await page.reload()
+    await asyncio.sleep(1)
+    await page.locator('xpath=/html/body/div[1]/div[1]/div/main/div[3]/div[1]/button[1]').click()
+    await page.locator('xpath=/html/body/div[1]/div[1]/div/main/div[3]/div[2]/div/div[2]/div/div/div[2]/div[3]/select').select_option(value ='Arial')
+    await page.locator('xpath=/html/body/div[1]/div[1]/div/main/div[3]/div[2]/div/div[2]/div/div/div[2]/div[4]/select').select_option(value='25px')
+    await page.locator('xpath=/html/body/div[1]/div[1]/div/main/div[3]/div[2]/div/div[2]/div/div/div[2]/div[5]/select').select_option(value='130%')
+    await page.close()
 
-async def download_missing_chapter(links):
-    results = []
-    setting = True
-    async with async_playwright() as p:
-        browser = await p.firefox.launch(headless=True)
-        page = await browser.new_page()
-        await page.goto('https://metruyencv.info/',timeout=0)
-        await page.locator('xpath=/html/body/div[1]/header/div/div/div[3]/button').click()
-        await asyncio.sleep(1)
-        await page.locator('xpath=/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div/div[1]/button').click()
-        await asyncio.sleep(1)
-        await page.locator('xpath=/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/input').fill(username)
-        await asyncio.sleep(1)
-        await page.locator('xpath=/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/input').fill(password)
-        await asyncio.sleep(1)
-        await page.locator('xpath=/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[3]/div[1]/button').click()
-        await asyncio.sleep(1)
-        await page.locator('xpath=/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[2]/button').click()
-        await asyncio.sleep(1)
-        for title,link,num in links:
-            await page.goto(link,timeout=0)
-            await page.route("**/*", handle_route)
-            if setting:
-                await page.locator('xpath=/html/body/div[1]/main/div[3]/div[1]/button[1]').click()
-                await page.locator('xpath=/html/body/div[1]/main/div[3]/div[2]/div/div[2]/div/div/div[2]/div[3]/select').select_option(value ='Times New Roman')
-                await page.locator('xpath=/html/body/div[1]/main/div[3]/div[2]/div/div[2]/div/div/div[2]/div[4]/select').select_option(value='50px')
-                await page.reload()
-                setting = False
-            loadmore_element1 = await page.wait_for_selector('xpath=/html/body/div[1]/main/div[4]/div[1]', state='attached')
-            missing_html1 = await loadmore_element1.inner_html()
-            missing_html1 = str(missing_html1)
-            if missing_html1.count('<br><br>') <= 4:
-                loadmore_element2 = await page.wait_for_selector('xpath=/html/body/div[1]/main/div[4]/div[3]',state='attached')
-                missing_html2 = await loadmore_element2.inner_html()
-                missing_html2 = BeautifulSoup(str(missing_html2),'lxml')
-                if missing_html2.find('canvas') != None:
-                    images = await page.query_selector_all('canvas')
-                    for image in images:
-                        image_bytes = await image.screenshot()
-                        ocr_result = ocr(image_bytes)
-                        missing_html2.find('canvas').replace_with(ocr_result)
-                missing_html1 = missing_html1 + '<br/><br/>' + str(missing_html2)
-                missing_html1 = missing_html1.replace('<br/><br/>', '<br/>').replace('<br/>', '<br/><br/>').replace('\n', ' ')
-                results.append((title,missing_html1,num))
-            print(f'Đã tải xong chap {num}')
-        await browser.close()
-    return results
+
+async def info(context,novel_url):
+    page = await context.new_page()
+    await page.goto(novel_url,timeout=0)
+    html = await page.content()
+    soup = BeautifulSoup(html,'lxml')
+    title = str(soup.find('h1', class_='mb-2').text)
+    author = str(soup.find('a', class_='text-gray-500').text).strip()
+    status = str(
+        soup.find('a', class_='inline-flex border border-primary rounded px-2 py-1 text-primary').select_one(
+            'span').text).strip()
+    attribute = str(soup.find('a',
+                                class_='inline-flex border border-rose-700 dark:border-red-400 rounded px-2 py-1 text-rose-700 dark:text-red-400').text)
+    
+    image_url = soup.find('img', class_='w-44 h-60 shadow-lg rounded mx-auto')['src']
+    desc = str(soup.find('div',class_='text-gray-600 dark:text-gray-300 py-4 px-2 md:px-1 text-base break-words').text)
+    return title, author, status, attribute, image_url,desc
+
+
+
+async def download_chapter(num, novel_url, context, asyncio_semaphore):
+    async with asyncio_semaphore:
+        link = f'{novel_url}/chuong-{num}'
+        page = await context.new_page()
+        await page.goto(link,timeout=0)
+        loadmore_element1 = await page.wait_for_selector('xpath=/html/body/div[1]/div[1]/div/main/div[4]/div[1]', state='visible')
+        content = await page.content()
+        soup = BeautifulSoup(content,'lxml')
+        title = str(soup.find('h2', class_='text-center text-gray-600 dark:text-gray-400 text-balance').text)
+        missing_html = await loadmore_element1.inner_html()
+        missing_html = BeautifulSoup(str(missing_html),'lxml')
+        images = await page.query_selector_all('canvas')
+        for image in images:
+            image_bytes = await image.screenshot()
+            ocr_result = await asyncio.to_thread(ocr, image_bytes)
+            missing_html.find('canvas').replace_with(ocr_result)
+        missing_html = str(missing_html).replace('<br/><br/>', '<br/>').replace('<br/>', '<br/><br/>').replace('\n', ' ').replace("·","")
+        await page.close()
+        return (title,missing_html,num)
 
 
 def sort_chapters(list_of_chapters):
@@ -154,69 +159,35 @@ def sort_chapters(list_of_chapters):
     return list_of_chapters
 
 
-# Retry decorator for handling transient errors, excluding 404 errors
-
-@backoff.on_exception(backoff.expo, httpx.HTTPError, max_tries=3, giveup=lambda e: e.response.status_code == 404)
-async def get_chapter_with_retry(chapter_number, novel_url):
-    url = f'{novel_url}/chuong-{chapter_number}'
-    i = 0
-    try:
-        while True:
-            resp = await client.get(url, headers=header)
-            resp.raise_for_status()
-            soup = BeautifulSoup(resp.content, 'lxml')
-            chapter_content = soup.find('div', class_='break-words')
-            chapter_title = str(soup.find('h2', class_='text-center text-gray-600 dark:text-gray-400 text-balance'))
-            html = str(chapter_content)
-            if html.count("<br/>") != 8:
-                break
-            else:
-                i += 1
-                if i == 10:
-                    missing_chapter.append((chapter_title,url,chapter_number))
-                    return None
-
-        if html is None or chapter_title is None:
-            print(f"""
-Lỗi: Không thể tìm thấy chapter {chapter_number}, đang bỏ qua...""")
-            return None
-
-        return chapter_title, html, chapter_number
-    except httpx.HTTPError as e:
-        if e.response.status_code == 404:
-            print(f"""
-Lỗi: Không thể tìm thấy chapter {chapter_number} (404), đang bỏ qua...""")
-            return None
-        else:
-            print(f"HTTP error fetching chapter {chapter_number}: {e}. Đang thử lại...")
-            await asyncio.sleep(5)
-            raise
-
-# Cache the results of the 'get' function for better performance
-@alru_cache(maxsize=1024)
 async def fetch_chapters(start_chapter, end_chapter, novel_url):
-    tasks1 = [get_chapter_with_retry(number, novel_url) for number in range(start_chapter, end_chapter + 1)]
-    chapters = []
-    # Use tqdm to display a progress bar
-    async for future1 in tqdm(asyncio.as_completed(tasks1), total=end_chapter - start_chapter + 1, desc="Tải chapters...",
-                             unit=" chapters"):
-        chapter = await future1  # Await here to get the actual result
-        if chapter is not None:
-            chapters.append(chapter)
-    print('Đang tải chapters bị thiếu...')
-    if missing_chapter != []:
-        chapters += await download_missing_chapter(missing_chapter)
+    async with async_playwright() as p:
+        print(f'Logining...')
+        browser = await p.chromium.launch(headless=True)
+        context = await browser.new_context()
+        await login(context,f'{novel_url}/chuong-{start_chapter}')
+        asyncio_semaphore = asyncio.Semaphore(max_tabs)
+        tasks1 = [download_chapter(number, novel_url, context,asyncio_semaphore) for number in range(start_chapter, end_chapter + 1)]
+        chapters = []
+        # Use tqdm to display a progress bar
+        async for future in tqdm(asyncio.as_completed(tasks1), total=end_chapter - start_chapter + 1, desc="Tải chapters...",
+                                unit=" chapters"):
+            chapter = await future  # Await here to get the actual result
+            if chapter is not None:
+                chapters.append(chapter)
     chapters = delete_dupe(chapters)
     sorted_chapters = sort_chapters(chapters)
     return sorted_chapters
 
 
-def create_epub(title, author, status, attribute, image, chapters, path, filename):
+def create_epub(title, author, status, attribute, image, chapters, path, filename,desc):
     book = epub.EpubBook()
     book.set_title(title)
     book.set_identifier("nguyentd010")
     book.add_author(author)
-    book.set_language('vn')
+    book.set_language('vi')
+    book.add_metadata('DC', 'creator', 'nguyentd010')
+    book.add_metadata('DC', 'publisher', 'nguyentd010')
+    book.add_metadata('DC', 'description', desc)
     book.add_metadata(None, 'meta', '', {'name': 'status', 'content': status})
     book.add_metadata(None, 'meta', '', {'name': 'chapter', 'content': str(len(chapters))})
     book.set_cover(content=image, file_name='cover.jpg')
@@ -265,30 +236,13 @@ async def main():
             novel_url = novel_url[:-1]
         start_chapter = int(input('Chapter bắt đầu: '))
         end_chapter = int(input('Chapter kết thúc: '))
-
-        try:
-            response = await client.get(novel_url, headers=header)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.content, 'lxml')
-            title = str(soup.find('h1', class_='mb-2').text)
-            author = str(soup.find('a', class_='text-gray-500').text).strip()
-            status = str(
-                soup.find('a', class_='inline-flex border border-primary rounded px-2 py-1 text-primary').select_one(
-                    'span').text).strip()
-            attribute = str(soup.find('a',
-                                      class_='inline-flex border border-rose-700 dark:border-red-400 rounded px-2 py-1 text-rose-700 dark:text-red-400').text)
-            image_url = soup.find('img', class_='w-44 h-60 shadow-lg rounded mx-auto')['src']
-        except (httpx.HTTPError, TypeError, KeyError) as e:
-            print(f"Error fetching novel information: {e}")
-            continue
-
-        try:
-            image_data = await client.get(image_url, headers=header)
-            image_data.raise_for_status()
-            image = image_data.content
-        except httpx.HTTPError as e:
-            print(f"Error downloading image: {e}")
-            continue
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=True)
+            context = await browser.new_context()
+            title, author, status, attribute, image_url,desc = await info(context,novel_url)
+        image_data = await client.get(image_url, headers=header)
+        image_data.raise_for_status()
+        image = image_data.content
 
         filename = novel_url.replace(BASE_URL, '').replace('-', '')
         path = f"{disk}:/novel/{title.replace(':', ',').replace('?', '')}"
@@ -298,7 +252,7 @@ async def main():
         valid_chapters = [chapter for chapter in chapters if chapter is not None]
 
         if valid_chapters:
-            create_epub(title, author, status, attribute, image, valid_chapters, path, filename)
+            create_epub(title, author, status, attribute, image, valid_chapters, path, filename,desc)
             print(
                 f'Tải thành công {len(valid_chapters)}/{end_chapter - start_chapter + 1} chapter. File của bạn nằm tại "D:/novel"')
         else:
